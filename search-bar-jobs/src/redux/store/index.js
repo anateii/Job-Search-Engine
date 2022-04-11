50aee61f413a58e2463a963b5a8456f00fb64461
Ana Matei <anamatei.099@gmail.com>
2022-04-11 13:53:18 +0100

    Added Security Layer + Reload Storage

--- search-bar-jobs/src/redux/store/index.js
+++ search-bar-jobs/src/redux/store/index.js
@@ -1,21 +1,45 @@
 import { createStore } from "redux";
 import mainReducer from "../reducers";
+import { persistReducer, persistStore } from 'redux-persist'
+import localStorage from 'redux-persist/lib/storage'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
 
-export const initialState = localStorage.getItem("STORE")
-  ? JSON.parse(localStorage.getItem("STORE"))
-  : {
+export const initialState = //localStorage.getItem("STORE")
+  //? JSON.parse(localStorage.getItem("STORE")):
+   {
       companies: {
         favourites: [],
       },
     };
 
-const configureStore = createStore(
+
+ const persistConfig = {
+      key: 'root',
+      storage: localStorage,
+      transforms: [
+        encryptTransform({
+          secretKey: process.env.REACT_APP_SECRET_KEY,
+          // you should not write the secretKey here in the code
+          onError: (error) => {
+            console.log(error)
+          },
+        }),
+      ],
+    }
+
+
+
+const persistedReducer = persistReducer(persistConfig, mainReducer)
+
+
+
+export const configureStore = createStore(
   // 1) the main reducer of the application (or the only one)
-  mainReducer,
+  persistedReducer,
   // 2) the initial state for your redux store
   initialState,
   // 3) an enhancer function, for extensions or middlewares
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default configureStore;
+export const persistor = persistStore(configureStore)
